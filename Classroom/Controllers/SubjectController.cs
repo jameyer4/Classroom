d6778c40e7747ae154675145dc76900d3811bd85
arnom <arnomeye@gmail.com>
2016-02-23 15:55:32 +0200

    Improved main chart display according to logged in user constraints

--- Classroom/Controllers/SubjectController.cs
+++ Classroom/Controllers/SubjectController.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using Classroom.Models;
+using Microsoft.Ajax.Utilities;
+using NSubstitute;
+using NSubstitute.Core;
 
 
 namespace Classroom.Controllers
@@ -31,8 +37,23 @@ namespace Classroom.Controllers
 #region ChartMain
         public ActionResult ChartMain()
         {
+            ViewBag.DataFlag = false;
+            if (User.Identity.Name.IsNullOrWhiteSpace())
+            {
+                ViewBag.DataFlag = true;            
+            }
+            Subjects subjects = new Subjects();
+            ///Needs optimisation
+            bool studMarks = db.Subject.Any(s => s.Afrikaans>0 && db.Student.Select(x=>x.User.Equals(User.Identity.Name)&&x.Id.Equals(s.StudentId)).Distinct().Count()>1);
+            ViewBag.hasMarks = studMarks;
+               return View();
+            
+        }
+
+        public ActionResult Chart()
+        {
             var subjects = db.Subject.ToList();
-            var students = db.Student.Where(s=>s.User.Equals(User.Identity.Name)).ToList();
+            var students = db.Student.Where(s => s.User.Equals(User.Identity.Name)).ToList();
             ViewBag.errorMessage = "No student subject data to display. ";
             ViewBag.dataFlag = false;
 
@@ -40,33 +61,50 @@ namespace Classroom.Controllers
             List<double> averages = new List<double>();
             List<string> names = new List<string>();
             int count = 0;
+            bool flag = false;
             foreach (var x in subjects)
             {
-                var studId = students[count].Id;
-                if (x.StudentId.Equals(studId))
+                try
                 {
-                    eng = x.English;
-                    afrik = x.Afrikaans;
-                    math = x.Math;
-                    geog = x.Geography;
-                    hist = x.History;
-                    life = x.LifeOrientation;
-                    nat = x.NaturalScience;
-                    sum = afrik + eng + life + math + nat + geog + hist;
-                    averages.Add(sum/7);
-                    var stud = db.Student.Find(x.StudentId);
-                    names.Add(stud.FirstName + " " + stud.LastName);
+                    var studId = students[count].Id;
+                    if (x.StudentId.Equals(studId))
+                    {
+                        eng = x.English;
+                        afrik = x.Afrikaans;
+                        math = x.Math;
+                        geog = x.Geography;
+                        hist = x.History;
+                        life = x.LifeOrientation;
+                        nat = x.NaturalScience;
+                        sum = afrik + eng + life + math + nat + geog + hist;
+                        averages.Add(sum/7);
+                        var stud = db.Student.Find(x.StudentId);
+                        names.Add(stud.FirstName + " " + stud.LastName);
+                    }
+                    count++;
                 }
-                count++;
+                catch (Exception ex)
+                {
+                    //log issue or get better handling for no student ID error
+                    flag = true;
+                    break;
+                }
             }
 
             ViewBag.Averages = averages;
             ViewBag.Names = names;
 
-            if (names.Count < 1)
+            if (names.Count < 1||flag)
             {
                 ViewBag.dataFlag = true;//There is no data to display
+                return View();
             }
+
+            var chart =new Chart(width: 600, height: 400, theme: ChartTheme.Vanilla)
+                .AddTitle("Student Averages")
+                .AddSeries(name: "Student marks", xValue: names, yValues: averages)
+                .Write();
+
             return View();
         }
 #endregion
