dd76a75853924410c0d019cbbfe70f66058e0f60
arnom <arnomeye@gmail.com>
2016-03-11 13:13:08 +0200

    Update 11/03/2016

---
+++ Classroom/Models/DB_Models/TeacherSubjects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Classroom.Models.DB_Models
+{
+    public class TeacherSubjects
+    {
+        public int Id { get; set; }
+        public int TeacherId { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
